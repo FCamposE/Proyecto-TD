bdb75568022499b13876a9686f94d8fdf95987c1
zAlvaroxD <68798925+zAlvaroxD@users.noreply.github.com>
2023-07-06 17:04:24 -0500

    Add files via upload
    modificado para activar la animacion de la muerte del enemigo se crea la variable isDie como false que luego se volvera true cuando el enemigo se quede sin vida.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -14,6 +14,9 @@ public class Enemy : MonoBehaviour
 
 
     public Transform[] movementPoints;
+    public Animator animator;
+    private bool isDie = false;
+
     private int currentPointIndex = 0;
 
     private GM gameManager;
@@ -21,6 +24,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         gameManager = FindObjectOfType<GM>();
         speed = 2;
         initialHealth = 50;
@@ -33,6 +37,12 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (isDie)
+        {
+            // Detener el movimiento del enemigo si está muerto
+            speed = 0;
+            return;
+        }
         //Debug.Log(currentHealth);
         if (transform.position == movementPoints[currentPointIndex].position)
         {
@@ -41,23 +51,47 @@ public class Enemy : MonoBehaviour
 
         transform.position = Vector3.MoveTowards(transform.position, movementPoints[currentPointIndex].position, speed * Time.deltaTime);
 
-        if (currentPointIndex == movementPoints.Length-1)
+        /*if (currentPointIndex == movementPoints.Length-1)
         {
             Destroy(gameObject, 10f);
             //Debug.Log("destruir");
-        }
+        }*/
     }
     public void TakeDamage(int damageAmount)
     {
+        if (isDie) return;
         currentHealth -= damageAmount;
 
         if (currentHealth <= 0)
         {
+            Die();
+        }
+        
+        /*if (currentHealth <= 0)
+        {
             // El enemigo ha sido destruido
+
             Destroy(gameObject);
             gameManager.AddCoins(10);
-        }
+        }*/
     }
+
+    void Die()
+    {
+        isDie = true;
+        animator.SetBool("isDie", true);
+
+        // Desactivar colisiones y movimiento al morir
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+
+        // Realizar acciones adicionales al morir
+        gameManager.AddCoins(10);
+
+        // Destruir el objeto después de un tiempo (opcional)
+        Destroy(gameObject, 1.3f);
+    }
+
     public void IncreaseStats(float speed, int health){
         this.currentHealth += health;
         this.speed += speed;
